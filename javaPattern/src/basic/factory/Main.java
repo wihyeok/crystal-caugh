e2bb1b55f79f8ff031f9a2937179a4439202e4e8
wihyeok <honey.hyeok@gmail.com>
2015-12-21 15:09:50 +0900

    factory method pattern

---
+++ javaPattern/src/basic/factory/Main.java
@@ -0,0 +1,18 @@
+package basic.factory;
+
+import basic.factory.framework.*;
+import basic.factory.idcard.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Factory factory = new IDCardFactory();
+		Product card1 = factory.create("HONEY");
+		Product card2 = factory.create("BEE");
+		Product card3 = factory.create("BEAR");
+		card1.use();
+		card2.use();
+		card3.use();
+	}
+
+}
---
+++ javaPattern/src/basic/factory/idcard/IDCardFactory.java
@@ -0,0 +1,20 @@
+package basic.factory.idcard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import basic.factory.framework.*;
+
+public class IDCardFactory extends Factory {
+	private List<String> owners = new ArrayList<String>();
+	protected Product createProduct(String owner) {
+		return new IDCard(owner);
+	}
+	protected void registerProduct(Product product) {
+		owners.add(((IDCard)product).getOwner());
+	}
+	public List<String> getOwners() {
+		return owners;
+	}
+
+}
