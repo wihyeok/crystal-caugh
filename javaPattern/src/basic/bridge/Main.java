3dd59b84c08a7f302e9232dfdb1239a1e9722f1a
wihyeok <honey.hyeok@gmail.com>
2016-09-10 01:43:03 +0900

    Bridge

--- javaPattern/src/basic/bridge/Main.java
+++ javaPattern/src/basic/bridge/Main.java
@@ -13,7 +13,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		Display d1 = new Display(new StringDisplayImpl("Hello, Korea."));
-
+		Display d2 = new CountDisplay(new StringDisplayImpl("Hello, Korea."));
 	}
 
 }
