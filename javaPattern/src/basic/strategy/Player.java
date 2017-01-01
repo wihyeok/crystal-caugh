bdd6a409b5c1b69cdbaebb208ab9e17c3ee70bf0
wihyeok <honey.hyeok@gmaill.com>
2017-01-02 00:22:12 +0900

    Strategy pattern

--- javaPattern/src/basic/strategy/Player.java
+++ javaPattern/src/basic/strategy/Player.java
@@ -1,5 +1,46 @@
 package basic.strategy;
 
 public class Player {
-
+	private String name;
+	private Strategy strategy;
+	private int wincount;
+	private int losecount;
+	private int gamecount;
+	
+	
+	/**
+	 * <pre>
+	 * 플레이어의 이름과 전략을 할당 받는다.
+	 * </pre>
+	 * @param name
+	 * @param strategy
+	 */
+	public Player(String name, Strategy strategy) {
+		this.name = name;
+		this.strategy = strategy;
+	}
+	
+	public Hand nextHand() {
+		return strategy.nextHand();
+	}
+	
+	public void win() {
+		strategy.study(true);
+		wincount++;
+		gamecount++;
+	}
+	
+	public void lose() {
+		strategy.study(false);
+		losecount++;
+		gamecount++;
+	}
+	
+	public void even() {
+		gamecount++;
+	}
+	
+	public String toString() {
+		return "[" + name + ":" + gamecount + " games, " + wincount + " win, " + losecount + " lose" + "]";
+	}
 }
