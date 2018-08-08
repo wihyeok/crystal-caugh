71b87cb489c5617e364fc13d75e686b6ce243382
wihyeok <honey.hyeok@gmail.com>
2018-08-08 10:53:39 +0900

    state pattern

---
+++ javaPattern/src/basic/state/Context.java
@@ -0,0 +1,8 @@
+package basic.state;
+
+public interface Context {
+	public abstract void setClock(int hour);
+	public abstract void changeState(State state);
+	public abstract void callSecurityCenter(String msg);
+	public abstract void recordLog(String msg);
+}
