ae1aa099ecfa7cc14ef939d44af3523a4a49cc25
hyeinrose <gn00090@naver.com>
2018-08-22 15:11:01 +0000

    Update Main.java

--- javaPattern/src/khiPattern/templateMethod/Main.java
+++ javaPattern/src/khiPattern/templateMethod/Main.java
@@ -1,5 +1,14 @@
 package khiPattern.templateMethod;
 
+
+/*
+templateMethod 따라해보고 여기에 계속 붙여서 다른패턴도 적용해볼려구요!!! 
+
+그런데 withdraw랑 deposit에 
+코드가 중복대자나요...저건 따로 안빼는게 맞는가....ㅠㅠㅠ
+
+
+*/
 public class Main {
 
 	public static void main(String[] args) {
