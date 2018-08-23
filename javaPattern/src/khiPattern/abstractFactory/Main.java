dc9ef88d68e8ae0c87656e77a4e4f698a906862c
honeyhyeok <honeyhyeok@hyeokui-mbp>
2018-08-24 01:45:53 +0900

    abstract facgtory pattern

---
+++ javaPattern/src/khiPattern/abstractFactory/Main.java
@@ -0,0 +1,17 @@
+package khiPattern.abstractFactory;
+
+/**
+ * 추상팩토리 메드는 말로는 이해시켜드리기가 조금 까다로운 패턴입니다.
+ * 이건 내일 잘 되어있는 예제를 알려드릴께요
+ * 추상팩토리 패턴의 정리는 제가 내일까지 정리해서 올리겠습니다.
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
