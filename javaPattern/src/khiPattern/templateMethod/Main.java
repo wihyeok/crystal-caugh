b732bee8c2e43bc7601124ab9bfb8b155eab8122
honeyhyeok <honeyhyeok@hyeokui-mbp>
2018-08-27 01:53:12 +0900

    abstract pattern

--- javaPattern/src/khiPattern/templateMethod/Main.java
+++ javaPattern/src/khiPattern/templateMethod/Main.java
@@ -1,5 +1,12 @@
 package khiPattern.templateMethod;
 
+/**
+ * 
+ * !템플릿 메서드 패턴은 전체적으로는 동일하지만 부분적으로는 다른 구문으로 구성된 메서드의 코드 중복을 최소화할 때 유용합니다.
+ * 따라서 바뀌는 것과 바뀌지 않는 것을 분리하는 것이 중요합니다.
+ *
+ */
+
 public class Main {
 
 	public static void main(String[] args) {
