e6ca7e377df1fc36ce60b8ee365e5f4142263a3b
honeyhyeok <honeyhyeok@hyeokui-mbp>
2018-09-06 01:31:25 +0900

    khi study mediator pattern wh proxy pattern

---
+++ javaPattern/src/basic/proxy/Main.java
@@ -0,0 +1,22 @@
+package basic.proxy;
+
+/**
+ * 
+ * proxy - 대리인
+ * 
+ * 본인이 필요하게되는 특정상황(PrintProxy 클래스가 print메소드를 실행하는 상황)이 오기 전까지
+ * 대리인(PrintProxy 클래스)은 대리인의 역할을 계속 수행한다.
+ * 
+ * 
+ * 
+ *
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
