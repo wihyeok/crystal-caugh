af33c2a1c1c0221a87d0f164539c2592777da23b
wihyeok <honey.hyeok@gmail.com>
2018-09-28 10:04:03 +0900

    memento pattern main

---
+++ javaPattern/src/khiPattern/memento/Main.java
@@ -0,0 +1,19 @@
+package khiPattern.memento;
+
+/**
+ * 텍트에디터 같은 곳에서 undo, redo 등의 기능을 구현할 때 사용하기도 하는 패턴이기도 하며
+ * 인스턴스 정보를 캡슐화하는 방법까지 학습할 수 있는 패턴입니다.
+ * 공부한거 올려주세요~ 트렐로에서 다 하셨으면 done으로 바꿔주세요~
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
